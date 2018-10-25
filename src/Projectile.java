6b7f5450b91fae92e72b899af862264d6aed9977
Rachel Elberger <relberge@student.touro.edu>
2018-10-25 13:14:52 -0400

    shooter and projectile

--- src/Projectile.java
+++ src/Projectile.java
@@ -1,2 +1,22 @@
-public class Projectile {
+public class Projectile
+{
+	double angle;
+	double velocity;
+
+	public Projectile(double angle, double velocity)
+	{
+		this.angle = angle;
+		this.velocity = velocity;
+	}
+
+	public double getX(double time)
+	{
+		return Math.cos(Math.toRadians(angle)) * velocity * time;
+	}
+
+	public double getY(double time)
+	{
+		return Math.sin(Math.toRadians(angle)) * velocity * time -
+				(.5 * 9.8 * time * time);
+	}
 }
--- src/Shooter.java
+++ src/Shooter.java
@@ -1,2 +1,55 @@
-public class Shooter {
+import java.awt.*;
+
+public class Shooter
+{
+	private boolean exists;
+	private Image shooterIcon;
+	private Projectile projectile;
+	private int location; //location can be an x coordinate...?
+
+	public Shooter(boolean exists, int location)
+	{
+		this.exists = exists;
+		this.location = location;
+	}
+
+	public boolean isExists()
+	{
+		return exists;
+	}
+
+	public void setExists(boolean exists)
+	{
+		this.exists = exists;
+	}
+
+	public Image getShooterIcon()
+	{
+		return shooterIcon;
+	}
+
+	public void setShooterIcon(Image shooterIcon)
+	{
+		this.shooterIcon = shooterIcon;
+	}
+
+	public Projectile getProjectile()
+	{
+		return projectile;
+	}
+
+	public void setProjectile(Projectile projectile)
+	{
+		this.projectile = projectile;
+	}
+
+	public int getLocation()
+	{
+		return location;
+	}
+
+	public void setLocation(int location)
+	{
+		this.location = location;
+	}
 }
